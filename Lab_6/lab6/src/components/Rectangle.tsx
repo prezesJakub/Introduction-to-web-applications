b5f21de15dbd9f01fdf39f376475311ae39d4883
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2025-01-09 02:32:10 +0100

    Lab6 colloquium solution added

---
+++ Lab_6/lab6/src/components/Rectangle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Rectangle: React.FC = () => {
+    return <div className="shape rectangle"></div>;
+};
+
+export default Rectangle;
---
+++ Lab_6/lab6/src/components/Square.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Square: React.FC = () => {
+    return <div className="shape square"></div>;
+};
+
+export default Square;
